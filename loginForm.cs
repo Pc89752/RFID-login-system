171a11cec11dfd08509af110a1f4772816efbe46
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2023-06-26 05:33:17 +0800

    contain into namespace screen_lock

--- loginForm.cs
+++ loginForm.cs
@@ -1,38 +1,41 @@
-public class LoginForm : TableLayoutPanel
+namespace screen_lock
 {
-    public LoginForm()
+    public class LoginForm : TableLayoutPanel
     {
-        // Set explicit size for the TableLayoutPanel
-        Width = 250;
+        public LoginForm()
+        {
+            // Set explicit size for the TableLayoutPanel
+            Width = 250;
 
-        Label lblUsername = new Label();
-        lblUsername.Text = "Username:";
-        Controls.Add(lblUsername, 0, 0);
+            Label lblUsername = new Label();
+            lblUsername.Text = "Username:";
+            Controls.Add(lblUsername, 0, 0);
 
-        TextBox txtUsername = new TextBox();
-        Controls.Add(txtUsername, 1, 0);
+            TextBox txtUsername = new TextBox();
+            Controls.Add(txtUsername, 1, 0);
 
-        Label lblPassword = new Label();
-        lblPassword.Text = "Password:";
-        Controls.Add(lblPassword, 0, 1);
+            Label lblPassword = new Label();
+            lblPassword.Text = "Password:";
+            Controls.Add(lblPassword, 0, 1);
 
-        TextBox txtPassword = new TextBox();
-        txtPassword.PasswordChar = '*';
-        Controls.Add(txtPassword, 1, 1);
+            TextBox txtPassword = new TextBox();
+            txtPassword.PasswordChar = '*';
+            Controls.Add(txtPassword, 1, 1);
 
-        Button btnLogin = new Button();
-        btnLogin.Text = "Login";
-        Controls.Add(btnLogin, 1, 2);
+            Button btnLogin = new Button();
+            btnLogin.Text = "Login";
+            Controls.Add(btnLogin, 1, 2);
+        }
+
+        // [STAThread]
+        // static void Main()
+        // {
+        //     Application.EnableVisualStyles();
+        //     Application.SetCompatibleTextRenderingDefault(false);
+        //     Form mainForm = new Form();
+        //     mainForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+        //     mainForm.Controls.Add(new LoginForm());
+        //     Application.Run(mainForm);
+        // }
     }
-
-    // [STAThread]
-    // static void Main()
-    // {
-    //     Application.EnableVisualStyles();
-    //     Application.SetCompatibleTextRenderingDefault(false);
-    //     Form mainForm = new Form();
-    //     mainForm.FormBorderStyle = FormBorderStyle.FixedDialog;
-    //     mainForm.Controls.Add(new LoginForm());
-    //     Application.Run(mainForm);
-    // }
 }
--- loginScreen.cs
+++ loginScreen.cs
@@ -8,75 +8,78 @@ using System.Text;
 using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
 
-public class LoginScreen : Form
+namespace screen_lock
 {
-    private LoginForm? _loginForm;
-    private Label _errorLabel = new Label();
-    public LoginScreen()
+    public class LoginScreen : Form
     {
-        InitializeComponent();
-    }
-
-    private void InitializeComponent()
-    {
-        this.SuspendLayout();
-
-        this.ControlBox=false;
-        this.WindowState = FormWindowState.Maximized;
-        this.FormBorderStyle = FormBorderStyle.None;
-        this.TopMost = true;
-
-        TableLayoutPanel loginPanel = new TableLayoutPanel();
-        loginPanel.Dock = DockStyle.Top;
-        loginPanel.AutoSize = true;
-        loginPanel.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-        loginPanel.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-
-        // adding loginForm
-        _loginForm = new LoginForm();
-        _loginForm.Anchor = AnchorStyles.None;
-        if(Screen.PrimaryScreen!=null)
-            _loginForm.Margin = new Padding(0, Screen.PrimaryScreen.Bounds.Height/4, 0, 0);
-
-        // adding errorLabel
-        // _errorLabel.Text = "Form cannot be closed manually!";
-        _errorLabel.ForeColor = Color.Red;
-        _errorLabel.Font = new Font("Arial", 24,FontStyle.Bold);
-        _errorLabel.Margin = new Padding(0, 30, 0, 0);
-        _errorLabel.AutoSize = true;
-        _errorLabel.Anchor = AnchorStyles.None;
-        _errorLabel.TextAlign = ContentAlignment.MiddleCenter;
-
-        loginPanel.Controls.Add(_loginForm, 0, 0);
-        loginPanel.Controls.Add(_errorLabel, 0, 1);
-        loginPanel.SetColumnSpan(_errorLabel, 1);
-
-        Controls.Add(loginPanel);
-
-        // TODO: uncomment this line before officially run
-        // this.FormClosing += preventUserClosing;
-        this.ResumeLayout(false);
-        this.PerformLayout();
-    }
-
-    private void preventUserClosing(object? sender, FormClosingEventArgs e)
-    {
-        // Check the CloseReason
-        if (e.CloseReason == CloseReason.UserClosing)
+        private LoginForm? _loginForm;
+        private Label _errorLabel = new Label();
+        public LoginScreen()
         {
-            e.Cancel = true;
-            _errorLabel.Text = "Form cannot be closed manually!";
+            InitializeComponent();
         }
-    }
 
-    [STAThread]
-    static void Main()
-    {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
 
-        LoginScreen loginScreen = new LoginScreen();
-        loginScreen.Padding = new Padding(50);
-        Application.Run(loginScreen);
+            this.ControlBox=false;
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+
+            TableLayoutPanel loginPanel = new TableLayoutPanel();
+            loginPanel.Dock = DockStyle.Top;
+            loginPanel.AutoSize = true;
+            loginPanel.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            loginPanel.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+
+            // adding loginForm
+            _loginForm = new LoginForm();
+            _loginForm.Anchor = AnchorStyles.None;
+            if(Screen.PrimaryScreen!=null)
+                _loginForm.Margin = new Padding(0, Screen.PrimaryScreen.Bounds.Height/4, 0, 0);
+
+            // adding errorLabel
+            // _errorLabel.Text = "Form cannot be closed manually!";
+            _errorLabel.ForeColor = Color.Red;
+            _errorLabel.Font = new Font("Arial", 24,FontStyle.Bold);
+            _errorLabel.Margin = new Padding(0, 30, 0, 0);
+            _errorLabel.AutoSize = true;
+            _errorLabel.Anchor = AnchorStyles.None;
+            _errorLabel.TextAlign = ContentAlignment.MiddleCenter;
+
+            loginPanel.Controls.Add(_loginForm, 0, 0);
+            loginPanel.Controls.Add(_errorLabel, 0, 1);
+            loginPanel.SetColumnSpan(_errorLabel, 1);
+
+            Controls.Add(loginPanel);
+
+            // TODO: uncomment this line before officially run
+            // this.FormClosing += preventUserClosing;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private void preventUserClosing(object? sender, FormClosingEventArgs e)
+        {
+            // Check the CloseReason
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                _errorLabel.Text = "Form cannot be closed manually!";
+            }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            LoginScreen loginScreen = new LoginScreen();
+            loginScreen.Padding = new Padding(50);
+            Application.Run(loginScreen);
+        }
     }
 }
