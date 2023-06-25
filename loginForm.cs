356986a192ace1083536392987ed9f84a8a847f6
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2023-06-26 05:05:10 +0800

    Finished the login screen

--- loginForm.cs
+++ loginForm.cs
@@ -1,61 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-
-public class Form1 : Form
+public class LoginForm : TableLayoutPanel
 {
-    private TextBox accountTxtBox = new System.Windows.Forms.TextBox();
-    private TextBox passwordTxtBox = new System.Windows.Forms.TextBox();
-
-    public Form1()
+    public LoginForm()
     {
-        InitializeComponent();
+        // Set explicit size for the TableLayoutPanel
+        Width = 250;
+
+        Label lblUsername = new Label();
+        lblUsername.Text = "Username:";
+        Controls.Add(lblUsername, 0, 0);
+
+        TextBox txtUsername = new TextBox();
+        Controls.Add(txtUsername, 1, 0);
+
+        Label lblPassword = new Label();
+        lblPassword.Text = "Password:";
+        Controls.Add(lblPassword, 0, 1);
+
+        TextBox txtPassword = new TextBox();
+        txtPassword.PasswordChar = '*';
+        Controls.Add(txtPassword, 1, 1);
+
+        Button btnLogin = new Button();
+        btnLogin.Text = "Login";
+        Controls.Add(btnLogin, 1, 2);
     }
 
-    private void InitializeComponent()
-    {
-        this.SuspendLayout();
-
-        this.ClientSize = new System.Drawing.Size(284, 264);
-        // this.Controls.Add(this.passwordTxtBox);
-        this.ControlBox=false;
-        this.WindowState = FormWindowState.Maximized;
-        this.FormBorderStyle = FormBorderStyle.None;
-        this.TopMost = true;
-
-        (var cw, var ch) = (Screen.PrimaryScreen.Bounds.Width/2, Screen.PrimaryScreen.Bounds.Height/2);
-        this.accountTxtBox.Location = new Point(cw, ch);
-        this.accountTxtBox.Width = cw/2;
-        this.Controls.Add(this.accountTxtBox);
-        // this.FormClosing += preventUserClosing;
-        // this.Text = "TextBox Example";
-        this.ResumeLayout(false);
-        this.PerformLayout();
-    }
-
-    private void preventUserClosing(object? sender, FormClosingEventArgs e)
-    {
-        // Check the CloseReason to determine if it's a manual close
-        if (e.CloseReason == CloseReason.UserClosing)
-        {
-            // Cancel the close operation
-            e.Cancel = true;
-            
-            // Optionally, display a message to inform the user
-            MessageBox.Show("Form cannot be closed manually.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-        }
-    }
-
-    [STAThread]
-    static void Main()
-    {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new Form1());
-    }
+    // [STAThread]
+    // static void Main()
+    // {
+    //     Application.EnableVisualStyles();
+    //     Application.SetCompatibleTextRenderingDefault(false);
+    //     Form mainForm = new Form();
+    //     mainForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+    //     mainForm.Controls.Add(new LoginForm());
+    //     Application.Run(mainForm);
+    // }
 }
---
+++ loginScreen.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+public class LoginScreen : Form
+{
+    private LoginForm? _loginForm;
+    private Label _errorLabel = new Label();
+    public LoginScreen()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        this.SuspendLayout();
+
+        this.ControlBox=false;
+        this.WindowState = FormWindowState.Maximized;
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.TopMost = true;
+
+        TableLayoutPanel loginPanel = new TableLayoutPanel();
+        loginPanel.Dock = DockStyle.Top;
+        loginPanel.AutoSize = true;
+        loginPanel.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+        loginPanel.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+
+        // adding loginForm
+        _loginForm = new LoginForm();
+        _loginForm.Anchor = AnchorStyles.None;
+        if(Screen.PrimaryScreen!=null)
+            _loginForm.Margin = new Padding(0, Screen.PrimaryScreen.Bounds.Height/4, 0, 0);
+
+        // adding errorLabel
+        // _errorLabel.Text = "Form cannot be closed manually!";
+        _errorLabel.ForeColor = Color.Red;
+        _errorLabel.Font = new Font("Arial", 24,FontStyle.Bold);
+        _errorLabel.Margin = new Padding(0, 30, 0, 0);
+        _errorLabel.AutoSize = true;
+        _errorLabel.Anchor = AnchorStyles.None;
+        _errorLabel.TextAlign = ContentAlignment.MiddleCenter;
+
+        loginPanel.Controls.Add(_loginForm, 0, 0);
+        loginPanel.Controls.Add(_errorLabel, 0, 1);
+        loginPanel.SetColumnSpan(_errorLabel, 1);
+
+        Controls.Add(loginPanel);
+
+        // TODO: uncomment this line before officially run
+        // this.FormClosing += preventUserClosing;
+        this.ResumeLayout(false);
+        this.PerformLayout();
+    }
+
+    private void preventUserClosing(object? sender, FormClosingEventArgs e)
+    {
+        // Check the CloseReason
+        if (e.CloseReason == CloseReason.UserClosing)
+        {
+            e.Cancel = true;
+            _errorLabel.Text = "Form cannot be closed manually!";
+        }
+    }
+
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+
+        LoginScreen loginScreen = new LoginScreen();
+        loginScreen.Padding = new Padding(50);
+        Application.Run(loginScreen);
+    }
+}
