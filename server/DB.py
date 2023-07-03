2906987b1cd0ee86aa4b115093db2b6760a517ee
CloudStarMarks <david1hiang@gmail.com>
2023-07-03 15:23:27 +0800

    U

--- server/DB.py
+++ server/DB.py
@@ -6,15 +6,13 @@ parent = str(Path(__file__).parent.absolute())
 db_path = os.path.join(parent, "DB.db")
 conn = sqlite3.connect(db_path)
 
-def recreate_student_table(source_json_path):
+def recreate_student_table(cursor, source_json_path):
     import json
     
     jsonData = json.load(open(source_json_path, "r"))
     if not jsonData:
         print(f"{source_json_path}: wrong format!")
         return
-    
-    cursor = conn.cursor()
     try:
         cursor.execute(f"DROP TABLE {jsonData['table_name']}")
     except:
@@ -29,11 +27,51 @@ def recreate_student_table(source_json_path):
     cursor.execute(cmmdStr)
     
     conn.commit()
-    conn.close()
+
+def recreate_arrivalList_table(cursor, source_json_path): 
+    import json
+    jsonData = json.load(open(source_json_path, "r"))
+    if not jsonData:
+        print(f"{source_json_path}: wrong format!")
+        return
+    
+    try:
+        cursor.execute(f"DROP TABLE {jsonData['table_name']}")
+    except:
+        pass
+    cursor.execute(f"CREATE TABLE {jsonData['table_name']} ({','.join([' '.join(e) for e in jsonData['cols']])})")
+
+    
+    conn.commit()
+
+def recreate_readCard_table(cursor, source_json_path): 
+    import json
+    jsonData = json.load(open(source_json_path, "r"))
+    if not jsonData:
+        print(f"{source_json_path}: wrong format!")
+        return
+    
+    try:
+        cursor.execute(f"DROP TABLE {jsonData['table_name']}")
+    except:
+        pass
+    cursor.execute(f"CREATE TABLE {jsonData['table_name']} ({','.join([' '.join(e) for e in jsonData['cols']])})")
+    cmmdStr = f"INSERT INTO {jsonData['table_name']} ({','.join([e[0] for e in jsonData['cols']])}) VALUES "
+    for row in jsonData["rows"]:
+        row = [f"'{e}'" for e in row]
+        cmmdStr += "(" + ",".join(row) + "),"
+    cmmdStr = cmmdStr[0:-1]
+    cursor.execute(cmmdStr)
+    
+    conn.commit()
 
 if __name__ == "__main__":
     import json
-    recreate_student_table(os.path.join(parent, "student_account.json"))
+    cursor = conn.cursor()
+    recreate_student_table(cursor, os.path.join(parent, "student_account.json"))
+    recreate_arrivalList_table(cursor, os.path.join(parent, "arrivalList.json"))
+    recreate_readCard_table(cursor, os.path.join(parent,"readCard.json"))
+    conn.close()
     # jsonData = json.load(open(os.path.join(parent, "student_account.json"), "r"))
     # cmmdStr = f"INSERT INTO {jsonData['table_name']} ({','.join([e[0] for e in jsonData['cols']])}) VALUES "
     # for row in jsonData["rows"]:
