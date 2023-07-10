1bf7beb229768eb73e62a76e586a8c66d5c902a3
CloudStarMarks <david1hiang@gmail.com>
2023-07-10 15:28:07 +0800

    insert_touple

--- server/DB.py
+++ server/DB.py
@@ -6,7 +6,7 @@ parent = str(Path(__file__).parent.absolute())
 db_path = os.path.join(parent, "DB.db")
 conn = sqlite3.connect(db_path)
 
-def recreate_student_table(source_json_path):
+def recreate_table(source_json_path):
     import json
     
     jsonData = json.load(open(source_json_path, "r"))
@@ -22,19 +22,39 @@ def recreate_student_table(source_json_path):
     
     cursor.execute(f"CREATE TABLE {jsonData['table_name']} ({','.join([' '.join(e) for e in jsonData['cols']])})")
     cmmdStr = f"INSERT INTO {jsonData['table_name']} ({','.join([e[0] for e in jsonData['cols']])}) VALUES "
-    for row in jsonData["rows"]:
-        row = [f"'{e}'" for e in row]
-        cmmdStr += "(" + ",".join(row) + "),"
-    cmmdStr = cmmdStr[0:-1]
-    cursor.execute(cmmdStr)
+    if jsonData["rows"]:
+        for row in jsonData["rows"]:
+            row = [f"'{e}'" for e in row]
+            cmmdStr += "(" + ",".join(row) + "),"
+        cmmdStr = cmmdStr[0:-1]
+        cursor.execute(cmmdStr)
     
     conn.commit()
-    conn.close()
+    
 
+def insert_touples_into_database(ID,enter_time,leave_time):
+    # conn = sqlite3.connect('DB.db')
+    cursor = conn.cursor()
+    
+    table_name = 'ComputerUsage'
+
+    # 將元組資料插入資料庫
+    try:
+        cursor.execute(f"INSERT INTO {table_name} VALUES ({ID},{enter_time},{leave_time} )")
+        conn.commit()
+        print("成功插入元組到資料庫")
+    except Exception as e:
+        print("插入元組失敗:", str(e))
+    
+    # 關閉連線
+ 
 
 if __name__ == "__main__":
     import json
-    recreate_student_table(os.path.join(parent, "student_account.json"))
+    recreate_table(os.path.join(parent, "student_account.json"))
+    recreate_table(os.path.join(parent, "computer_usage.json"))
+    insert_touples_into_database(12356,12358,456789)
+    conn.close()
     # jsonData = json.load(open(os.path.join(parent, "student_account.json"), "r"))
     # cmmdStr = f"INSERT INTO {jsonData['table_name']} ({','.join([e[0] for e in jsonData['cols']])}) VALUES "
     # for row in jsonData["rows"]:
