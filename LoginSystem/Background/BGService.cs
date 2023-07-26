66bcb627f0d53afd87583a1e1fa9a66174d0aa0f
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2023-07-26 16:29:47 +0800

    Implement pipe in Backgound program

--- LoginSystem/Background/BGService.cs
+++ LoginSystem/Background/BGService.cs
@@ -1,81 +1,119 @@
-using System;
+// using System;
 using System.ServiceProcess;
 using System.Diagnostics;
-using System.Threading;
-using System.Management;
+using Serilog;
+using System.IO.Pipes;
 
-namespace LoginSystem
+namespace BGService
 {
     public class BGService: ServiceBase
     {
         // TODO: Get the Uri of server
-        private const string _serverUrl = "http://127.0.0.1:5000/closeReport/";
+        private const string _serverUrl = @"http://127.0.0.1:5000/closeReport/";
+        public static ILogger logger;
         // TODO: Get the computer ID
-        private const string _computerID = "MyComputer";
-        // set .exe path of UI
+        private const string _computerID = @"MyComputer";
+        const string LOG_FOLDER = @"/LoginSystem/log";
+        private const string PIPE_NAME = "LoginSystem_UI";
+        // private const string exe_path = @"C:\Program Files\LoginSystem\LoginUI.exe";
         private const string exe_path = @"C:\Users\91a04\OneDrive\文件\GitHub\RFID-login-system\LoginSystem\UI\bin\Debug\net7.0-windows\LoginUI.exe";
-        private static int? usageRecordID;
-        public BGService()
+        private static int usageRecordID = -1;
+        private static Thread thread_reciveRecordID = new Thread(reciveReordID);
+        static BGService()
         {
+            string logFolder = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments) + LOG_FOLDER;
+            logger = new LoggerConfiguration()
+                .WriteTo.File($"{logFolder}/{{Date}}.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
         }
 
+        // TODO: test pipeline
         protected override void OnStart(string[] args)
         {
-            // start the UI
-            Process process = new Process();
-            process.StartInfo.FileName = exe_path;
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            process.Start();
+            logger.Information("Background starting");
+            startUI();
+            logger.Information("Background started");
+        }
 
-            // TODO: get the usageRecordID from UI
-            process.WaitForExit();
-            usageRecordID = process.ExitCode;
+        private static void startUI()
+        {
+            logger.Information("UI Starting");
+            ProcessStartInfo psi = new ProcessStartInfo(exe_path);
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+            psi.CreateNoWindow = true;
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            psi.WorkingDirectory = Path.GetDirectoryName(exe_path);
+            psi.EnvironmentVariables["pipe_name"] = PIPE_NAME;
+
+            try
+            {
+                using (Process process = new Process())
+                {
+                    process.StartInfo = psi;
+                    process.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "An exception occurred during starting UI.");
+            }
+            logger.Information("UI Started");
+        }
+
+        private static void reciveReordID()
+        {
+            Console.WriteLine("Recieving");
+            usageRecordID = PipeHandler.ReceiveDataAsync(PIPE_NAME);
+            Console.WriteLine($"Recieved: {usageRecordID}");
         }
 
         protected override void OnStop()
         {
-            // TODO: get usageRecordID
-            if(usageRecordID != null) returnClosing(Convert.ToInt32(usageRecordID));
+            logger.Information("Background closing");
+            if(usageRecordID != -1) returnClosing(usageRecordID);
             cleaning();
+            logger.Information("Background closed");
         }
 
-        private void cleaning()
+        private static void cleaning()
         {
         }
+
 
         private async void returnClosing(int usageRecordID)
         {
-            using(var client = new HttpClient())
+            try
             {
-                try
+                using(var client = new HttpClient())
                 {
-                    await client.PostAsync(_serverUrl, new StringContent(Convert.ToString(usageRecordID)));
-                }
-                catch (System.Exception ex)
-                {
-                    Log.log("ERROR", "Post closing message", ex, null);
+                    try
+                    {
+                        await client.PostAsync(_serverUrl, new StringContent(Convert.ToString(usageRecordID)));
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex, "An exception occurred during BGService.returnClosing");
+                    }
                 }
             }
-        }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "An exception occurred during BGService.OnStop");
+            }
 
+        }
+        
         [STAThread]
         static void Main()
         {
-            // start the UI
-            Process process = new Process();
-            process.StartInfo.FileName = exe_path;
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            process.Start();
-
-            // TODO: get the usageRecordID from UI
-            process.WaitForExit();
-            usageRecordID = process.ExitCode;
+            // startUI();
+            thread_reciveRecordID.Start();
+            thread_reciveRecordID.Join();
             // XXX: Testing
-            Console.WriteLine(usageRecordID);
+            // Console.WriteLine(usageRecordID);
         }
+
     }
 }
---
+++ LoginSystem/Background/PipeHandler.cs
@@ -0,0 +1,31 @@
+using System.IO.Pipes;
+using Serilog;
+
+namespace BGService
+{
+    class PipeHandler
+    {
+        public static int ReceiveDataAsync(string pipe_name)
+        {
+            var pipeServer = new NamedPipeServerStream(pipe_name, PipeDirection.In);
+            pipeServer.WaitForConnectionAsync().Wait();
+            var reader = new StreamReader(pipeServer);
+            while (true)
+            {
+                // Read data from the named pipe asynchronously
+                int value = -1;
+                try
+                {
+                    value = pipeServer.ReadByte();
+                    pipeServer.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    BGService.logger.Error("An error occured during reading value from pipe", ex.ToString());
+                    Thread.CurrentThread.Interrupt();
+                }
+                return value;
+            }
+        }
+    }
+}
