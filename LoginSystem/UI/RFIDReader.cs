f529c9804b0e042b913a75053cbe55336411533b
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2023-07-27 17:02:11 +0800

    Update RFIDReader.cs Change RFIDReader.cs from threading to asynchronous

--- LoginSystem/UI/RFIDReader.cs
+++ LoginSystem/UI/RFIDReader.cs
@@ -13,6 +13,8 @@ namespace LoginUI
         private string _exePath = Settings.NFcCode_path;
         private Process? _process;
         private ServerHandler _sh;
+        private CancellationTokenSource? cancellationTokenSource;
+        private Task? readingTask;
         private bool _processRunning = false;
         public RFIDReader(ServerHandler sh)
         {
@@ -42,84 +44,51 @@ namespace LoginUI
 
         public void CloseProcess()
         {
-            if(_process == null) return;
-            Console.WriteLine($"Closing Process {_process.ProcessName}.");
-            try
+            if (cancellationTokenSource != null)
             {
-                _process.Kill();
-                Console.WriteLine($"Process {_process.ProcessName} closed successfully.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error while closing the process: {ex.Message}");
+                cancellationTokenSource.Cancel();
+                cancellationTokenSource.Dispose();
+                cancellationTokenSource = null;
+                _processRunning = false;
+                Console.WriteLine("Stopped Reading");
             }
         }
         public void startReading()
         {
-            if(_processRunning) return;
-            
+            if (_processRunning) return;
+
             _process = Process.Start(_exePath);
             _processRunning = true;
-            _thread = new Thread(() => readInput());
-            _thread.Start();
-            Console.WriteLine("");
+            cancellationTokenSource = new CancellationTokenSource();
+            readingTask = readInputAsync(cancellationTokenSource.Token);
+            Console.WriteLine("Started Reading");
         }
 
-        private async void readInput()
+        private async Task readInputAsync(CancellationToken cancellationToken)
         {
-            while(true)
+            while (!cancellationToken.IsCancellationRequested)
             {
                 string? inp = await Task.Run(() => Console.ReadLine());
-                if(inp!=null)
+                if (inp != null)
                 {
                     _innerCode = inp;
                     Console.WriteLine(inp);
-                    _stateLabel.Text = "loging in...";
+                    _stateLabel.Text = "logging in...";
                     _stateLabel.ForeColor = Color.Black;
-                    // await submit(sh);
-                    submit();
+                    await submitAsync();
                 }
-                Thread.Sleep(2000);
+                await Task.Delay(2000, cancellationToken);
             }
         }
 
-        private async void submit()
+        private async Task submitAsync()
         {
             Dictionary<string, object> payload = new Dictionary<string, object>()
             {
                 {"innerCode", _innerCode}
             };
-            int status_code = await _sh.submit(payload, Settings.RFIDReader_endpoint);
-
-            switch(status_code)
-            {
-                case -1:
-                    _stateLabel.ForeColor = Color.Orange;
-                    _stateLabel.Text = "Connect failed!";
-                    break;
-                case 0:
-                    _stateLabel.ForeColor = Color.Blue;
-                    _stateLabel.Text = "Success!";
-                    break;
-                case 3:
-                    _stateLabel.ForeColor = Color.Red;
-                    _stateLabel.Text = "Invalid innerCode!";
-                    break;
-                default:
-                    Log.log("ERROR", $"status_code: {status_code}", new Exception("status_code out of range"), null);
-                    break;
-            }
+            (_stateLabel.ForeColor, _stateLabel.Text) = await _sh.submitAsync(payload, Settings.RFIDReader_endpoint);
+            await LoginUI.usageRecordID_loginAsync();
         }
-        // [STAThread]
-        // static void Main()
-        // {
-        //     Application.EnableVisualStyles();
-        //     Application.SetCompatibleTextRenderingDefault(false);
-        //     Form mainForm = new Form();
-        //     mainForm.FormBorderStyle = FormBorderStyle.FixedDialog;
-        //     mainForm.Controls.Add(new RFIDReader())1106173973
-
-        //     Application.Run(mainForm);
-        // }
     }
 }
