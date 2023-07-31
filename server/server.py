47714df4de66521944253558147f9f23d29b8eeb
CloudStarMarks <david1hiang@gmail.com>
2023-07-31 12:31:36 +0800

    update server

--- server/server.py
+++ server/server.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 # TODO: Refresh DEV_TOKEN once a while
 DEV_TOKEN = r"a!Qe*1!H/jgr8clv6yDQjaadi{I}C0B7Viwd}W1TsI=-f)lMWyHrm9V}Kx,2IF\1"
 PARDIR = str(Path(__file__).parent.absolute())
-RECORD_DB_PATH = os.path.join(PARDIR, "ComputerUsage.db")
+COMPUTER_USAGE_DB_PATH = os.path.join(PARDIR, "ComputerUsage.db")
 INFO_DB_PATH = os.path.join(PARDIR, "StudentINFO.db")
 
 @app.route('/submit/<action>', methods=['POST'])
 def submit(action):
     jsonData = json.loads(request.get_data(as_text=True))
-    record_db = DB(RECORD_DB_PATH)
+    record_db = DB(COMPUTER_USAGE_DB_PATH)
     info_db = DB(INFO_DB_PATH)
 
     status_code = None
@@ -41,39 +41,42 @@ def submit(action):
 @app.route('/closeReport', methods=['POST'])
 def handle_close_report():
     jsonData = json.loads(request.get_data(as_text=True))
-    record_db = DB(RECORD_DB_PATH)
+    record_db = DB(COMPUTER_USAGE_DB_PATH)
     
     usageRecordID =  jsonData["usageRecordID"]
     now = datetime.now()
     leaveTime = now.strftime("%Y-%m-%d, %H:%M:%S")
     record_db.update_DB("ComputerUsage", usageRecordID, [("leaveTime", leaveTime)])
 
-def handle_account_login(record_db, info_db, jsonData):
+def handle_account_login(computer_usage_db, info_db, jsonData):
     # get username and password
     cID, sID, password = jsonData["computerID"], jsonData["account"], jsonData["password"]
     # Check account and password
     valid_user, valid_password = False, False
     t = info_db.getTuple("StudentAccount", sID)
-    check = record_db.getTuple("computerUsage",sID)
-    if not check:
-        if t:
-            valid_user = True
-            if password == t[1]:
-                valid_password = True
-            
-        if not valid_user:
-            return 1, None
-        if not valid_password:
-            return 2, None
-        
-        usageRecordID = record_db.rowCount("ComputerUsage")
-        now = datetime.now()
-        loginTime = now.strftime("%Y-%m-%d, %H:%M:%S")
-        record_db.insertTuples("ComputerUsage", [[usageRecordID, cID, sID, loginTime, "null"]])
-        return 0, usageRecordID
-    return 6,None
+    check = computer_usage_db.getTuple("ComputerUsage",sID)
+    # print(check)
+    if check:
+        return 6,None
 
-def handle_innerCode_login(record_db, info_db, jsonData):
+    if t:
+        valid_user = True
+        if password == t[1]:
+            valid_password = True
+        
+    if not valid_user:
+        return 1, None
+    if not valid_password:
+        return 2, None
+    
+    usageRecordID = computer_usage_db.rowCount("ComputerUsage")
+    now = datetime.now()
+    loginTime = now.strftime("%Y-%m-%d, %H:%M:%S")
+    computer_usage_db.insertTuples("ComputerUsage", [[sID]])
+    computer_usage_db.insertTuples("records",[[usageRecordID, cID, sID, loginTime, "null"]])
+    return 0, usageRecordID
+
+def handle_innerCode_login(computer_usage_db, info_db, jsonData):
     # get username and password
     innerCode, cID = jsonData["innerCode"], jsonData["computerID"]
     
@@ -90,10 +93,11 @@ def handle_innerCode_login(record_db, info_db, jsonData):
         if not valid_innerCode or not sID:
             return 3, None
         
-        usageRecordID = record_db.rowCount("ComputerUsage")
+        usageRecordID = computer_usage_db.rowCount("ComputerUsage")
         now = datetime.now()
         loginTime = now.strftime("%Y-%m-%d, %H:%M:%S")
-        record_db.insertTuples("ComputerUsage", [[usageRecordID, cID, sID, loginTime, "null"]])
+        computer_usage_db.insertTuples("ComputerUsage", [[sID]])
+        computer_usage_db.insertTuples("records",[[usageRecordID, cID, sID, loginTime, "null"]])
         return 0, usageRecordID
     return 6,None
 
