04d14b0a868dcf8ad9415376f9fc3dc888f4de47
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2023-12-02 01:04:52 +0800

    update

--- client/UI/RFIDReader.cs
+++ client/UI/RFIDReader.cs
@@ -4,10 +4,13 @@ namespace LoginUI
 {
     public class RFIDReader : TableLayoutPanel
     {
-        private Label _stateLabel = new Label();
+        private Label _errorLabel = new Label();
         private string _innerCode = "";
         private string exePath = Settings.NFcCode_path;
+        private Button _btnLogin = new Button();
         private readonly bool processExisted;
+        private Label _key = new Label();
+        private TextBox _txtKey = new TextBox();
         private Process? process;
         private ServerHandler _sh;
         private CancellationTokenSource? cancellationTokenSource;
@@ -21,13 +24,31 @@ namespace LoginUI
             if(processExisted) LoginUI.logger.Information("Process existed");
 
             AutoSize = true;
-            _stateLabel.Font = new Font("Arial", 24,FontStyle.Bold);
-            _stateLabel.Margin = new Padding(0, 30, 0, 0);
-            _stateLabel.AutoSize = true;
-            _stateLabel.Anchor = AnchorStyles.None;
-            _stateLabel.TextAlign = ContentAlignment.MiddleCenter;
-            _stateLabel.Text = "Waiting";
-            Controls.Add(_stateLabel, 0, 0);
+            FlowLayoutPanel panel = new FlowLayoutPanel();
+
+            // AutoSize = true;
+            // _stateLabel.Font = new Font("Arial", 24,FontStyle.Bold);
+            // _stateLabel.Margin = new Padding(0, 30, 0, 0);
+            // _stateLabel.AutoSize = true;
+            // _stateLabel.Anchor = AnchorStyles.None;
+            // _stateLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // _stateLabel.Text = "Waiting";
+            // Controls.Add(_stateLabel, 0, 0);
+
+            Controls.Add(panel);
+            Controls.Add(_btnLogin);
+            _key.Margin = new Padding(0,0,0,0);
+            _key.Text = "內碼:";
+            _txtKey.PasswordChar = '*';
+            _btnLogin.Text = "Submit";
+            _btnLogin.Click += onSubmitAsync;
+            _txtKey.Width = 800;
+            Controls.Add(_key, 0, 0);
+            Controls.Add(_txtKey, 0, 1);
+            Controls.Add(_btnLogin, 0, 2);
+
+            _errorLabel.AutoSize = true;
+            Controls.Add(_errorLabel, 0, 3);
         }
 
         // TODO: check if the process is killed when current tab is not RFID
@@ -62,40 +83,52 @@ namespace LoginUI
             else LoginUI.logger.Information("Process is running");
 
             cancellationTokenSource = new CancellationTokenSource();
-            readingTask = readInputAsync(cancellationTokenSource.Token);
+            // readingTask = readInputAsync(cancellationTokenSource.Token);
             LoginUI.logger.Information("Started Reading");
         }
 
-        private void clearInput()
-        {
-            while(!string.IsNullOrEmpty(Console.ReadLine()));
-        }
-
-        private async Task readInputAsync(CancellationToken cancellationToken)
-        {            
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                string? inp = await Task.Run(() => Console.ReadLine());
-                if (inp != null)
-                {
-                    _innerCode = inp;
-                    _stateLabel.Text = "logging in...";
-                    _stateLabel.ForeColor = Color.Black;
-                    await submitAsync();
-                }
-                await Task.Delay(2000, cancellationToken);
-            }
-        }
-
-        private async Task submitAsync()
+        private async void onSubmitAsync(object? sender, EventArgs e)
         {
             Dictionary<string, object> payload = new Dictionary<string, object>()
             {
-                {"innerCode", _innerCode}
+                {"innerCode", _txtKey.Text}
             };
             bool isSuccess;
-            (isSuccess, _stateLabel.ForeColor, _stateLabel.Text) = await _sh.submitAsync(payload, Settings.RFIDReader_endpoint);
+            (isSuccess, _errorLabel.ForeColor, _errorLabel.Text) = await _sh.submitAsync(payload, Settings.RFIDReader_endpoint);
             if(isSuccess) await LoginUI.usageRecordID_ReportAsync();
         }
+
+
+        // private void clearInput()
+        // {
+        //     while(!string.IsNullOrEmpty(Console.ReadLine()));
+        // }
+
+        // private async Task readInputAsync(CancellationToken cancellationToken)
+        // {            
+        //     while (!cancellationToken.IsCancellationRequested)
+        //     {
+        //         string? inp = await Task.Run(() => Console.ReadLine());
+        //         if (inp != null)
+        //         {
+        //             _innerCode = inp;
+        //             _stateLabel.Text = "logging in...";
+        //             _stateLabel.ForeColor = Color.Black;
+        //             await submitAsync();
+        //         }
+        //         await Task.Delay(2000, cancellationToken);
+        //     }
+        // }
+
+        // private async Task submitAsync()
+        // {
+        //     Dictionary<string, object> payload = new Dictionary<string, object>()
+        //     {
+        //         {"innerCode", _innerCode}
+        //     };
+        //     bool isSuccess;
+        //     (isSuccess, _stateLabel.ForeColor, _stateLabel.Text) = await _sh.submitAsync(payload, Settings.RFIDReader_endpoint);
+        //     if(isSuccess) await LoginUI.usageRecordID_ReportAsync();
+        // }
     }
 }
