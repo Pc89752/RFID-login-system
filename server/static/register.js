b6f3a4ecdd5a1c4cf98dec771f1f12186c025cfd
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2024-03-24 22:23:04 +0800

    Update register website 1. Fix fetching problem (bad request, status code: 400) 2. Change route submit_data(POST) to register(POST) 3. Add alerts of invalid inputs 4. Alert the added student ID when submit successed

--- server/static/register.js
+++ server/static/register.js
@@ -6,23 +6,16 @@
 function checkStudentId(event) {
   event.preventDefault();
   const fd = new FormData(event.currentTarget);
-  //   var studentId = document.getElementById("student_id").value;
-  //   if (!/^[U][0-9]{8}$/.test(studentId)) {
-  //     alert("學號必須是 9 位數，且第一位字母必須是大寫的 U");
-  //     return false;
-  //   }
+  var studentId = fd.studentId;
+  if (!/^[U][0-9]{8}$/.test(studentId)) {
+    alert("學號必須是 9 位數，且第一位字母必須是大寫的 U");
+    return;
+  }
 
   fetch("/register", { method: "POST", body: fd })
     .then((res) => res.json())
     .then((json) => {
-        if(json.success)
-            alert(`註冊成功:\n學號:${json.student_id}`)
-        else alert("不可重複註冊學生證");
+      if (json.success) alert(`註冊成功:\n學號:${json.student_id}`);
+      else alert("不可重複註冊學生證");
     });
-
-  //   if ( === "true") {
-  //     alert("不可重複註冊學生證");
-  //     return false;
-  //   }
-  return true;
 }
